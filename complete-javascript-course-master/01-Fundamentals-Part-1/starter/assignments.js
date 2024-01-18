92a7b774df3db251bf968fccb84f109b8720644d
Brad Hojara <brad.hojara@gmail.com>
2024-01-18 15:01:26 -0500

    completed the first four modules

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,33 @@
+const country = "The United States of America";
+const continent = "North America";
+let population = 380;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+const isIsland = false;
+const language = "English";
+//isIsland = true;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+console.log(population / 2);
+
+population++;
+
+console.log(population);
+console.log(population > 6);
+console.log(population < 33);
+
+const description1 =
+  country +
+  " is in " +
+  continent +
+  ", and its " +
+  population +
+  " million people speak " +
+  language;
+console.log(description1);
