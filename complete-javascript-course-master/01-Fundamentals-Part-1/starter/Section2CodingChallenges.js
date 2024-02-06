5f36fe698a00e6f3735c959480b738a65ced748f
Brad Hojara <brad.hojara@gmail.com>
2024-02-06 14:56:52 -0500

    Completed coding challenge #3 (Section 1)

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/Section2CodingChallenges.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/Section2CodingChallenges.js
@@ -1,3 +1,4 @@
+/*
 const massMark = 78;
 const heightMark = 1.69;
 const massJohn = 92;
@@ -30,3 +31,27 @@ if (BMIMark > BMIJohn) {
   console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})`);
 } else
   console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})`);
+*/
+// const scoreDolphins = (96 + 108 + 89) / 3;
+// const scoreKoalas = (88 + 91 + 110) / 3;
+// if (scoreDolphins > scoreKoalas) {
+//   console.log("Dolphins win the trophy");
+// } else if (scoreDolphins < scoreKoalas) {
+//   console.log("Koalas win the trophy");
+// } else {
+//   console.log("Both win the trophy");
+// }
+
+const scoreDolphins = (97 + 112 + 101) / 3;
+const scoreKoalas = (109 + 95 + 106) / 3;
+console.log(`Dolphins: ${scoreDolphins} Koalas: ${scoreKoalas}`);
+
+if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
+  console.log("Dolphins win the trophy");
+} else if (scoreDolphins < scoreKoalas && scoreKoalas >= 100) {
+  console.log("Koalas win the trophy");
+} else if (scoreDolphins < 100 && scoreKoalas < 100) {
+  console.log("Neither team averaged over 100 points so nobody wins");
+} else {
+  console.log("TIE GAME! Both win the trophy");
+}
--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
@@ -8,7 +8,7 @@ console.log(population);
 
 const isIsland = false;
 const language = "English";
-//isIsland = true;
+
 console.log(typeof isIsland);
 console.log(typeof population);
 console.log(typeof country);
@@ -48,4 +48,28 @@ if (population > 33) {
       averagePopulation - population
     } million below average`
   );
+}
+
+console.log("9" - "5"); // -> ?
+console.log("19" - "13" + "17"); // -> ?
+console.log("19" - "13" + 17); // -> ?
+console.log("123" < 57); // -> ?
+console.log(5 + 6 + "4" + 9 - 4 - 2); // -> ?
+
+//Commenting out so that the prompt does not interfere with the rest of the assignments
+// const numNeighbors = Number(
+//   prompt("How many neighbor countries does your country have?")
+// );
+// if (numNeighbors === 1) {
+//   console.log("Only 1 border!");
+// } else if (numNeighbors > 1) {
+//   console.log("More than 1 border");
+// } else {
+//   console.log("No borders");
+//}
+
+if (language === "English" && population < 50 && !isIsland) {
+  console.log(`You should live in ${country} :)`);
+} else {
+  console.log(`${country} does not meet your criteria :(`);
 }
