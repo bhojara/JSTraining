f6a4bc59dcd41ca95da489091381898bb2647618
Brad Hojara <brad.hojara@gmail.com>
2024-02-15 16:16:54 -0500

    Completed Coding challenge #1 in section 2

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/assignments.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/assignments.js
@@ -12,3 +12,57 @@ const descGermany = describeCountry("Germany", 83, "Berlin");
 console.log(descFinland);
 console.log(descPortugal);
 console.log(descGermany);
+
+//function declaration
+function percentageOfWorld1(population) {
+  return (population / 7900) * 100;
+}
+
+const chinaPopulationPercentage = percentageOfWorld1(1441);
+const germanPopulationPercentage = percentageOfWorld1(83);
+const finalndPopulationPercentage = percentageOfWorld1(6);
+
+console.log(
+  chinaPopulationPercentage,
+  germanPopulationPercentage,
+  finalndPopulationPercentage
+);
+
+//function expression
+const percentageOfWorld2 = function (population) {
+  return (population / 7900) * 100;
+};
+
+const chinaPopulationPercentage2 = percentageOfWorld2(1441);
+const germanPopulationPercentage2 = percentageOfWorld2(83);
+const finlandPopulationPercentage2 = percentageOfWorld2(6);
+
+console.log(
+  chinaPopulationPercentage2,
+  germanPopulationPercentage2,
+  finlandPopulationPercentage2
+);
+
+//Arrow function
+const percentageOfWorld3 = (population) => (population / 7900) * 100;
+
+const chinaPopulationPercentage3 = percentageOfWorld3(1441);
+const germanPopulationPercentage3 = percentageOfWorld3(83);
+const finlandPopulationPercentage3 = percentageOfWorld3(6);
+
+console.log(
+  chinaPopulationPercentage3,
+  germanPopulationPercentage3,
+  finlandPopulationPercentage3
+);
+
+const describePopulation = function (country, population) {
+  const percentage = percentageOfWorld1(population);
+  const description = `${country} has ${population} million people, which is about ${percentage}% of the world.`;
+
+  console.log(description);
+};
+
+describePopulation("China", 1441);
+describePopulation("Finland", 6);
+describePopulation("Germany", 83);
--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -1,17 +1,24 @@
 "use strict";
+
 /*
+///////////////////////////////////////
+// Activating Strict Mode
 let hasDriversLicense = false;
 const passTest = true;
 
 if (passTest) hasDriversLicense = true;
-if (hasDriversLicense) console.log("I can drive :D)");
+if (hasDriversLicense) console.log('I can drive :D');
 
-// const interface = "Audio";
+// const interface = 'Audio';
 // const private = 534;
-*/
+
+
+///////////////////////////////////////
+// Functions
 function logger() {
-  console.log("My name is Jonas");
+  console.log('My name is Jonas');
 }
+
 // calling / running / invoking function
 logger();
 logger();
@@ -28,4 +35,68 @@ console.log(appleJuice);
 const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
 
-const num = Number("23");
+const num = Number('23');
+
+
+//Function declaration
+function calcAge1(birthYear) {
+  return 2037 - birthYear;
+}
+const age1 = calcAge1(1991);
+
+//Function expression
+const calcAge2 = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+const age2 = calcAge2(1991);
+
+console.log(age1, age2);
+
+
+//Arrow function
+const calcAge3 = (birthYear) => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  //  return retirement;
+  return `${firstName} has ${retirement} years until retirement`;
+};
+
+console.log(yearsUntilRetirement(1991, "Brad"));
+console.log(yearsUntilRetirement(1980, "Bob"));
+
+function cutFruitPieces(fruit) {
+  return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+  const applePieces = cutFruitPieces(apples);
+  const orangePieces = cutFruitPieces(oranges);
+
+  const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`;
+  return juice;
+}
+console.log(fruitProcessor(2, 3));
+*/
+
+const calcAge = function (birthyear) {
+  return 2037 - birthyear;
+};
+
+const yearsUntilRetirement = function (birthYear, firstName) {
+  const age = calcAge(birthYear);
+  const retirement = 65 - age;
+
+  if (retirement > 0) {
+    console.log(`${firstName} has ${retirement} years until retirement`);
+    return retirement;
+  } else console.log(`${firstName} has already retired!`);
+  return -1;
+};
+
+console.log(yearsUntilRetirement(1991, "Jonas"));
+console.log(yearsUntilRetirement(1950, "Mike"));
